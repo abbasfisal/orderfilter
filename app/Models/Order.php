7c33ece35ddba0c006e9895c6897d7cc9deab127
abbas <abbasmomeny1994@gmail.com>
2024-05-01 07:36:21 +0330

    feat: implement Order Filtering ;0

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\OrderFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $mobile_number
+ * @property string $national_code
+ * @property int $amount
+ * @property bool $status
+ */
+class Order extends Model
+{
+    use HasFactory;
+
+    //    public function guard(array $guarded)
+//    {
+//    }
+
+    protected static function newFactory(): OrderFactory
+    {
+        return OrderFactory::new();
+    }
+}
+
+
+
